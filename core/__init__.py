568f59ff480fd7a06dc8c8e26d95c3a714f592e5
Jan <jgruca@students.uni-mainz.de>
2025-07-24 18:31:58 +0200

    added data folder

--- core/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Core processing modules for the caption tool.
-"""
-
-from transcriber import Transcriber
-from segmenter import Segmenter  
-from renderer import CaptionRenderer
-
-__all__ = ["Transcriber", "Segmenter", "CaptionRenderer"]
